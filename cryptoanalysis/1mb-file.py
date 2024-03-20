19d39053051dc7107547d50d64aa2a1ec65b9dd6
Lewis <lewismomanyi34@gmail.com>
2024-03-20 18:20:21 +0300

    Cryptographic Analysis

--- cryptoanalysis/1mb-file.py
+++ cryptoanalysis/1mb-file.py
@@ -2,7 +2,7 @@ from faker import Faker
 
 fake = Faker()
 
-desired_size_bytes = 1024 * 1024  # 1 MB
+desired_size_bytes = 1024 * 1024 * 2 # 2 MB
 generated_text = ''
 
 while len(generated_text.encode('utf-8')) < desired_size_bytes:
@@ -11,4 +11,5 @@ while len(generated_text.encode('utf-8')) < desired_size_bytes:
 # Trim excess characters to meet desired size exactly
 generated_text = generated_text[:desired_size_bytes]
 
+print(generated_text)
 print("Generated text size:", len(generated_text.encode('utf-8')), "bytes")
