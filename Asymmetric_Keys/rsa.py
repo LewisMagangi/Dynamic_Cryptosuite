878016588ca71a7c0bb15984cd32c1f720b83205
Lewis <lewismomanyi34@gmail.com>
2024-03-20 12:59:08 +0300

    code update

--- Asymmetric_Keys/rsa.py
+++ Asymmetric_Keys/rsa.py
@@ -20,7 +20,7 @@ def encrypt_decrypt_rsa(original_message, key_size=2048):
     # Encrypt the message using the public key
     cipher = PKCS1_OAEP.new(public_key)
     encrypted_message = cipher.encrypt(original_message.encode())
-    print(f"Encrypted Message :{encrypted_message}")
+    print(encrypted_message)
 
     # Decrypt the message using the private key
     cipher = PKCS1_OAEP.new(private_key)
@@ -29,11 +29,13 @@ def encrypt_decrypt_rsa(original_message, key_size=2048):
     return decrypted_message.decode()
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python rsa.py <message> [key_size]")
-        sys.exit(1)
+    # Check if any arguments are provided
+    if len(sys.argv) > 1:
+        original_message = sys.argv[1]
+    else:
+        # Use a default message if no arguments provided
+        original_message = "Hi how are you ?"
 
-    original_message = sys.argv[1]
     key_size = int(sys.argv[2]) if len(sys.argv) > 2 else 2048
 
     # Perform encryption and decryption with RSA
@@ -43,5 +45,3 @@ if __name__ == "__main__":
         print("Decrypted message:", decrypted_message)
     except ValueError:
         print("Error: Invalid key size. Supported key sizes are 1024, 2048, 3072, or 4096.")
-        
-        
