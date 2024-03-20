420bb37de0c515134ffb932bc867717be745bd01
Lewis <lewismomanyi34@gmail.com>
2024-03-20 09:17:20 +0300

    Modifying the argument to take an argument as a the message to be signed

--- Digital_Signatures/rsa-dig.py
+++ Digital_Signatures/rsa-dig.py
@@ -1,22 +1,40 @@
-from Crypto.Signature import pkcs1_15
-from Crypto.Hash import SHA256
-from Crypto.PublicKey import RSA
+import sys
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.exceptions import InvalidSignature
 
-# Generate RSA key pair
-key = RSA.generate(2048)
+def sign_and_verify(message):
+    # Generate RSA key pair
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
 
-# Data to be signed
-data = b"Hello, this is some data to be signed."
+    # Convert message to bytes
+    data = message.encode('utf-8')
 
-# Sign the data
-hash_func = SHA256.new(data)
-signer = pkcs1_15.new(key)
-signature = signer.sign(hash_func)
+    # Sign the data
+    signature = private_key.sign(
+        data,
+        padding.PKCS1v15(),
+        hashes.SHA256()
+    )
 
-# Verify the signature
-verifier = pkcs1_15.new(key.publickey())
-try:
-    verifier.verify(hash_func, signature)
-    print("Signature is valid.")
-except (ValueError, TypeError):
-    print("Signature is invalid.")
+    # Verify the signature
+    try:
+        public_key = private_key.public_key()
+        public_key.verify(
+            signature,
+            data,
+            padding.PKCS1v15(),
+            hashes.SHA256()
+        )
+        print("Signature is valid.")
+    except InvalidSignature:
+        print("Signature is invalid.")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <message_to_be_signed>")
+        sys.exit(1)
+
+    message = sys.argv[1]
+    sign_and_verify(message)
