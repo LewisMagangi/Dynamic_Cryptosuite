f7970e9b5b96a1f9e9e2b1a30a7510d0c77bb2f2
Lewis <lewismomanyi34@gmail.com>
2024-03-27 11:50:32 +0300

    time analysis on secure has algorithms

--- cryptoanalysis/time/hash/secure-hashing-time-analy.py
+++ cryptoanalysis/time/hash/secure-hashing-time-analy.py
@@ -6,84 +6,120 @@ def sha224(data):
     start_time = time.time()
     hashed_data = hashlib.sha224(data.encode()).hexdigest()
     end_time = time.time()
-    print("Time taken for SHA-224:", end_time - start_time, "seconds")
+    total_time = end_time - start_time
+    print("Time taken for SHA-224:", total_time, "seconds")
+    with open('sha224.txt', 'w') as elapsed_time_file:
+        elapsed_time_file.write(str(total_time))
     return hashed_data
 
 def sha256(data):
     start_time = time.time()
     hashed_data = hashlib.sha256(data.encode()).hexdigest()
     end_time = time.time()
-    print("Time taken for SHA-256:", end_time - start_time, "seconds")
+    total_time = end_time - start_time
+    with open('sha256.txt', 'w') as elapsed_time_file:
+        elapsed_time_file.write(str(total_time))
+    print("Time taken for SHA-256:", total_time, "seconds")
     return hashed_data
 
 def sha384(data):
     start_time = time.time()
     hashed_data = hashlib.sha384(data.encode()).hexdigest()
     end_time = time.time()
-    print("Time taken for SHA-384:", end_time - start_time, "seconds")
+    total_time = end_time - start_time
+    with open('sha384.txt', 'w') as elapsed_time_file:
+        elapsed_time_file.write(str(total_time))
+    print("Time taken for SHA-384:", total_time, "seconds")
     return hashed_data
 
 def sha512(data):
     start_time = time.time()
     hashed_data = hashlib.sha512(data.encode()).hexdigest()
     end_time = time.time()
-    print("Time taken for SHA-512:", end_time - start_time, "seconds")
+    total_time = end_time - start_time
+    with open('sha512.txt', 'w') as elapsed_time_file:
+        elapsed_time_file.write(str(total_time))
+    print("Time taken for SHA-512:", total_time, "seconds")
     return hashed_data
 
 def sha512_224(data):
     start_time = time.time()
     hashed_data = hashlib.sha512(data.encode()).hexdigest()[:56]
     end_time = time.time()
-    print("Time taken for SHA-512/224:", end_time - start_time, "seconds")
+    total_time = end_time - start_time
+    with open('sha512_224.txt', 'w') as elapsed_time_file:
+        elapsed_time_file.write(str(total_time))
+    print("Time taken for SHA-512/224:", total_time, "seconds")
     return hashed_data
 
 def sha512_256(data):
     start_time = time.time()
     hashed_data = hashlib.sha512(data.encode()).hexdigest()[:64]
     end_time = time.time()
-    print("Time taken for SHA-512/256:", end_time - start_time, "seconds")
+    total_time = end_time - start_time
+    with open('sha512_256.txt', 'w') as elapsed_time_file:
+        elapsed_time_file.write(str(total_time))
+    print("Time taken for SHA-512/256:", total_time, "seconds")
     return hashed_data
 
 def sha3_224(data):
     start_time = time.time()
     hashed_data = hashlib.sha3_224(data.encode()).hexdigest()
     end_time = time.time()
-    print("Time taken for SHA3-224:", end_time - start_time, "seconds")
+    total_time = end_time - start_time
+    with open('sha3_224.txt', 'w') as elapsed_time_file:
+        elapsed_time_file.write(str(total_time))
+    print("Time taken for SHA3-224:", total_time, "seconds")
     return hashed_data
 
 def sha3_256(data):
     start_time = time.time()
     hashed_data = hashlib.sha3_256(data.encode()).hexdigest()
     end_time = time.time()
-    print("Time taken for SHA3-256:", end_time - start_time, "seconds")
+    total_time = end_time - start_time
+    with open('sha3_256.txt', 'w') as elapsed_time_file:
+        elapsed_time_file.write(str(total_time))
+    print("Time taken for SHA3-256:", total_time, "seconds")
     return hashed_data
 
 def sha3_384(data):
     start_time = time.time()
     hashed_data = hashlib.sha3_384(data.encode()).hexdigest()
     end_time = time.time()
-    print("Time taken for SHA3-384:", end_time - start_time, "seconds")
+    total_time = end_time - start_time
+    with open('sha3_384.txt', 'w') as elapsed_time_file:
+        elapsed_time_file.write(str(total_time))
+    print("Time taken for SHA3-384:", total_time, "seconds")
     return hashed_data
 
 def sha3_512(data):
     start_time = time.time()
     hashed_data = hashlib.sha3_512(data.encode()).hexdigest()
     end_time = time.time()
-    print("Time taken for SHA3-512:", end_time - start_time, "seconds")
+    total_time = end_time - start_time
+    with open('sha3_512.txt', 'w') as elapsed_time_file:
+        elapsed_time_file.write(str(total_time))
+    print("Time taken for SHA3-512:", total_time, "seconds")
     return hashed_data
 
 def shake128(data, output_length=32):
     start_time = time.time()
     hashed_data = hashlib.shake_128(data.encode()).hexdigest(output_length)
     end_time = time.time()
-    print("Time taken for SHAKE128:", end_time - start_time, "seconds")
+    total_time = end_time - start_time
+    with open('shake128.txt', 'w') as elapsed_time_file:
+        elapsed_time_file.write(str(total_time))
+    print("Time taken for SHAKE128:", total_time, "seconds")
     return hashed_data
 
 def shake256(data, output_length=32):
     start_time = time.time()
     hashed_data = hashlib.shake_256(data.encode()).hexdigest(output_length)
     end_time = time.time()
-    print("Time taken for SHAKE256:", end_time - start_time, "seconds")
+    total_time = end_time - start_time
+    with open('shake256.txt', 'w') as elapsed_time_file:
+        elapsed_time_file.write(str(total_time))
+    print("Time taken for SHAKE256:", total_time, "seconds")
     return hashed_data
 
 def read_file(file_name):
